b6bade4a1d09f350e176ba14a6ec4951b26fa539
Luan Vilela <luan.lopes@ufms.br>
2024-05-06 18:40:05 -0400

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['universe', 'universe/native'],
+  rules: {
+    'import/order': 0,
+    'react-native/no-inline-styles': 0,
+    'import/namespace': 0,
+    'no-duplicate-imports': 'error',
+    'react-hooks/rules-of-hooks': 'off',
+  },
+};
