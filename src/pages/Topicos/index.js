d6c3085999971c500f6ce13de138ddaa822b0afb
gpratto <gpratto@psgtecnologiaaplicada.com.br>
2024-05-17 16:46:34 -0400

    finalização dos ajustes das telas

--- src/pages/Topicos/index.js
+++ src/pages/Topicos/index.js
@@ -1,64 +1,87 @@
-import React, { useState } from "react";
-import { Center, Text, Button, VStack, Stack, HStack, NativeBaseProvider } from "native-base";
-import { ScrollView, Animated, ImageBackground, View } from "react-native";
-import styles from "./styles";
-import HeaderAdmin from "../../components/HeaderAdmin";
-
-const Home = () => {
-    const persons = [
-        {
-            id: "1",
-            imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJ6M7dJ518-Lo8n6KfqoqAFdzYVnsnJM3vf027usZ8&s",
-            descricao: "Where does it come from? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Where does it come from? Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of ",
-        },
-    ];
-    return (
-        <View style={styles.estilo.container}>
-            <ScrollView>
-                <Text style={styles.estilo.titulo}>{"\n"}Natureza é o segredo da felicidade</Text>
-                <ImageBackground source=
-                    {{ uri: persons[0].imagem }}
-                    resizeMode="cover" style={styles.estilo.image}>
-                </ImageBackground>
-                <Text>Por Luan Vilela - Campo Grande/MS</Text>
-                <Text>02/01/2010 Atualizado há 2 anos</Text>
-
-                <Text style={styles.estilo.subTitulo}>{"\n"}Natureza é o segredo da felicidade</Text>
-                <Text>{persons[0].descricao}</Text>
-                <Text style={styles.estilo.subTitulo}>{"\n"}Natureza é o segredo da felicidade</Text>
-                <Text>{persons[0].descricao}{"\n"}</Text>
-
-                <HStack width="100%">
-                    <Stack mb="2.5" mt="1.5" direction={{
-                        base: "row",
-                        md: "row"
-                    }} space={2} mx={{
-                        base: "auto",
-                        md: "0"
-                    }}>
-                        <Button size="sm" colorScheme="green">Cultura</Button>
-                        <Button size="sm" colorScheme="green">
-                            Saber
-                        </Button>
-                        <Button size="sm" colorScheme="green" >
-                            Saúde
-                        </Button>
-                    </Stack>
-                </HStack>
-            </ScrollView>
-        </View>
-    );
-};
+import React, { useEffect, useState } from 'react';
+import { Text, Button, VStack, Stack, HStack, NativeBaseProvider, Center } from 'native-base';
+import { ScrollView, ImageBackground, View } from 'react-native';
+import styles from './styles';
+import HeaderAdmin from '../../components/HeaderAdmin';
+import Services from '../../services/Services';
+import Footer from '../../components/Footer';
 
 export default ({ route }) => {
-    const { itemId } = route.params;
-    return (
-        <NativeBaseProvider>
-        <HeaderAdmin title={'Tópicos'} />     
-                <Center flex={1}>
-                    <Home />
-                    <Text>itemId: {JSON.stringify(itemId)}</Text>
-                </Center>
-        </NativeBaseProvider>
-    );
+  const { itemId } = route.params;
+  const [comments, setComments] = useState({ subTitulo: '', descricao: '', imagemPequena: '' });
+
+  useEffect(() => {
+    fetchComments();
+  }, []);
+
+  const fetchComments = async () => {
+    console.log(itemId);
+
+    try {
+      const response = await Services.getAllComment('/conteudo', itemId);
+      console.log(response.data);
+      setComments(response.data);
+    } catch (error) {
+      console.error('Erro ao buscar os comentários:', error);
+    }
+  };
+
+  return (
+    <NativeBaseProvider>
+      <HeaderAdmin title={'Tópicos'} />
+      <Center flex={1} top={5}>
+        <Home comments={comments} />
+      </Center>
+    </NativeBaseProvider>
+  );
+};
+
+const Home = ({ comments }) => {
+  return (
+    <View style={styles.estilo.container}>
+      <ScrollView>
+        <Text style={styles.estilo.titulo}>{comments.subTitulo}</Text>
+        <ImageBackground
+          source={{ uri: comments.imagemPequena }}
+          resizeMode="cover"
+          style={styles.estilo.image}></ImageBackground>
+        <Text>Por {comments.autor} - Campo Grande/MS</Text>
+        <Text>{comments.updated_at} Atualizado </Text>
+
+        <Text style={styles.estilo.subTitulo}>{comments.subDescricao}</Text>
+        <Text>{comments.descricao}</Text>
+
+        <Text style={styles.estilo.subTitulo}>{comments.subDescricao}</Text>
+        <Text>{comments.descricao}</Text>
+
+        {/* <HStack width="100%">
+          <Stack
+            mb="2.5"
+            mt="1.5"
+            direction={{
+              base: 'row',
+              md: 'row',
+            }}
+            space={2}
+            mx={{
+              base: 'auto',
+              md: '0',
+            }}>
+            <Button size="sm" colorScheme="green">
+              Cultura
+            </Button>
+            <Button size="sm" colorScheme="green">
+              Saber
+            </Button>
+            <Button size="sm" colorScheme="green">
+              Saúde
+            </Button>
+          </Stack>
+        </HStack> */}
+        <View>
+          <Footer />
+        </View>
+      </ScrollView>
+    </View>
+  );
 };
